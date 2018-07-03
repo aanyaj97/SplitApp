66460bcc3aba8881010adc95a28f23519cf514ef
Aanya Jhaveri <aanya@Aanyas-MacBook-Pro.local>
2018-07-03 15:24:03 -0500

    separated Playground into filesystem

---
+++ SplitApp/Models/EvenSplitFunc.swift
@@ -0,0 +1,9 @@
+//
+//  EvenSplitFunc.swift
+//  SplitApp
+//
+//  Created by Aanya Jhaveri on 7/3/18.
+//  Copyright © 2018 Aanya Jhaveri. All rights reserved.
+//
+
+import Foundation
---
+++ SplitApp/Models/SplitInfo.swift
@@ -0,0 +1,9 @@
+//
+//  SplitInfo.swift
+//  SplitApp
+//
+//  Created by Aanya Jhaveri on 7/3/18.
+//  Copyright © 2018 Aanya Jhaveri. All rights reserved.
+//
+
+import Foundation
